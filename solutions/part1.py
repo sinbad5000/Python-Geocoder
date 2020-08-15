968ec09b99059a8faf0950678aa8a07257c34305
sinav <sinavameghi@gmail.com>
2020-08-15 10:05:56 -0700

    I think this was the code we went over in class

--- solutions/part1.py
+++ solutions/part1.py
@@ -1,7 +1,7 @@
-import requests
 import geocoder
 
-destinations = ["The Space Needle",
+destinations = [
+  "The Space Needle",
   "Crater Lake",
   "The Golden Gate Bridge",
   "Yosemite National Park",
@@ -12,12 +12,14 @@ destinations = ["The Space Needle",
   "Yellowstone National Park",
   "Sandpoint, Idaho",
   "Banff National Park",
-  "Capilano Suspension Bridge"]
+  "Capilano Suspension Bridge"
+  ]
 
 for point in destinations:
   # Get the latitude and longitude from `geocoder`.
   loc = geocoder.arcgis(point)
-
+  
+  temp = result['currently']['temperature']
   # Print out `geopy`'s results.
   print("{0} is located at ({1:.4f}, {2: .4f})".format(point, loc.latlng[0], loc.latlng[1]))
 
